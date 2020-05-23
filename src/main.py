6e46ea72d1c0f4f9d445fc9790d6d3b48370c052
Mahdi Chaari <mahdi-chaari@hotmail.com>
2020-05-23 17:32:06 -0400

    The main() function creates a DataFrame from the shapefile and creates LocationGrid object to plot the initial data graph while leaving all its relevant information accessible.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
+import shapefile as shp
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from classes.location_grid import LocationGrid
 
 
 def main():
-    print("Hello World")
+    shapefile_path = "./../resources/crime_dt"
+    df = None
+
+    with shp.Reader(shapefile_path, "r", encoding="ANSI") as sf:
+        fields = [x[0] for x in sf.fields][1:]
+        records = [y[:] for y in sf.records()]
+        #records = sf.records()
+        points = [s.points[0] for s in sf.shapes()]
+        df = pd.DataFrame(columns=fields, data=records)
+        df = df.assign(Coordinates=points)
+
+    lg = LocationGrid(shapefile_path)
+    lg.show()
 
 
 if __name__ == "__main__":
